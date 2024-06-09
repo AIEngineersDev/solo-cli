748a9f61e351a6cfb2882f883b052da4f78899c2
ddiddi <ddiddi>
2024-06-09 13:34:30 -0700

    feat: update with new commands

--- solo_cli/utils.py
+++ solo_cli/utils.py
@@ -3,6 +3,19 @@ import platform
 import requests
 import subprocess
 from tqdm import tqdm
+import GPUtil
+import psutil
+
+# Model data
+models = [
+    {"name": "LLaVA 1.5", "memory": 3.97, "license": "LLaMA 2", "file": "llava-v1.5-7b-q4.llamafile"},
+    {"name": "TinyLlama-1.1B", "memory": 2.05, "license": "Apache 2.0", "file": "TinyLlama-1.1B-Chat-v1.0.F16.llamafile"},
+    {"name": "Mistral-7B-Instruct", "memory": 3.85, "license": "Apache 2.0", "file": "mistral-7b-instruct-v0.2.Q4_0.llamafile"},
+    {"name": "Phi-3-mini-4k-instruct", "memory": 7.67, "license": "Apache 2.0", "file": "Phi-3-mini-4k-instruct.F16.llamafile"},
+    {"name": "Mixtral-8x7B-Instruct", "memory": 30.03, "license": "Apache 2.0", "file": "mixtral-8x7b-instruct-v0.1.Q5_K_M.llamafile"},
+    {"name": "WizardCoder-Python-34B", "memory": 22.23, "license": "LLaMA 2", "file": "wizardcoder-python-34b-v1.0.Q5_K_M.llamafile"},
+    {"name": "WizardCoder-Python-13B", "memory": 7.33, "license": "LLaMA 2", "file": "wizardcoder-python-13b.llamafile"}
+]
 
 def download_file(url, filename):
     if os.path.exists(filename):
@@ -30,3 +43,20 @@ def set_permissions(filename):
         os.rename(filename, new_filename)
         return new_filename
     return filename
+
+def get_system_gpu_memory():
+    gpus = GPUtil.getGPUs()
+    if not gpus:
+        return 0
+    return max(gpu.memoryTotal for gpu in gpus)
+
+def get_system_memory():
+    return psutil.virtual_memory().total / (1024 ** 3)  # Convert bytes to GB
+
+def recommend_model(gpu_memory, system_memory):
+    recommended_models = [model for model in models if model["memory"] <= gpu_memory and model["memory"] <= system_memory]
+    if not recommended_models:
+        return "No models can be supported with the current system configuration."
+    
+    recommended_models.sort(key=lambda x: x["memory"])
+    return recommended_models
